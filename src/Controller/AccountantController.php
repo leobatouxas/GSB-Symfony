7829a75f75a192f321e46362c1ba74f0cded6e61
leobatouxas <leo.batouxas@gmail.com>
2021-11-14 23:25:13 +0100

    Ajout: Cloturation fiche frais mois antérieur

--- src/Controller/AccountantController.php
+++ src/Controller/AccountantController.php
@@ -6,6 +6,7 @@ use App\Entity\FeeSheet;
 use App\Form\FeesheetType;
 use App\Repository\StateRepository;
 use App\Repository\FeeSheetRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,25 +16,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AccountantController extends AbstractController
 {
     #[Route('/accountant/feesheet', name: 'app_accountant_feesheet')]
-    public function index(FeeSheetRepository $feeSheetRepository, StateRepository $stateRepository): Response
+    public function index(EntityManagerInterface $em, FeeSheetRepository $feeSheetRepository, StateRepository $stateRepository, Request $request): Response
     {
-        $state = $stateRepository->find(2);
-        $feesheetsCreates = $feeSheetRepository->findBy(['state' => $state]);
+        //Récupération des fiche de frais aevc l'état cloturée
+        $stateCloturees = $stateRepository->find(2);
+        $feesheetsCloturees = $feeSheetRepository->findBy(['state' => $stateCloturees]);
+
+        //Formulaire Cloturation des fiches frais du mois dernier dont l'état est 'crée"
+        $form = $this->createFormBuilder()
+                    ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $date = new DateTime(date('Y-m-01 00:00:00'));
+            $date->modify('-1 month');
+            $stateCreate = $stateRepository->find(1);
+            $feesheetsCreates = $feeSheetRepository->findBy(['date' => $date,'state' => $stateCreate]);
+            foreach($feesheetsCreates as $feesheet)
+            {
+                $feesheet->setState($stateCloturees);
+                $em->persist($feesheet);
+                $em->flush();
+            }
+        }
+
         return $this->render('accountant/feesheet/index.html.twig', [
-            'feesheetsCreates' => $feesheetsCreates,
+            'feesheetsCloturees' => $feesheetsCloturees,
+            'form' => $form->createView()
         ]);
     }
 
     #[Route('/accountant/feesheet/{id<[0-9]+>}', name: 'app_accountant_feesheet_show')]
     public function show(FeeSheet $feesheet,Request $request, EntityManagerInterface $em, StateRepository $stateRepository): Response
     {
-        $form = $this->createForm(FeesheetType::class, $feesheet, [
-            'label'=> false
+
+            $form = $this->createForm(FeesheetType::class, $feesheet, [
+                'label'=> false
             ]);
 
             $form->handleRequest($request);
 
-            // On verifie si le formulaire à était submit, valide et que l'état de la fiche de frais est équivalent à 1 (crée).
+            // On verifie si le formulaire à était submit, valide et que l'état de la fiche de frais est équivalent à 2 (cloturée).
             if ($form->isSubmitted() && $form->isValid() && $feesheet->getState()->getId() === 2) {
                 
                 //Calcul montant Valide
@@ -41,7 +67,7 @@ class AccountantController extends AbstractController
                 foreach($feesheet->getStandardFeesLines() as $standardfeesline) {
                     $ValidAmount = $ValidAmount + $standardfeesline->getQuantity() * $standardfeesline->getStandardFees()->getUnitAmount();
                 }
-                
+
                 foreach($feesheet->getVariableFeesLines() as $variablefeesline){
                     //Calcul montant Valide
                     $ValidAmount = $ValidAmount + $variablefeesline->getAmount();
