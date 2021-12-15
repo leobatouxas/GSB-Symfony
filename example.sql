a3261ce8d223bd1318ca852d4c82cf5b01599941
leobatouxas <leo.batouxas@gmail.com>
2021-12-15 10:11:40 +0100

    Maj : Problème contrainte résolue

--- example.sql
+++ example.sql
@@ -6,14 +6,21 @@ INSERT INTO `employee_type` (`id`, `name`) VALUES
 	(1, 'Visiteur'),
 	(2, 'Comptable');
 
-INSERT INTO `fee_sheet` (`id`, `state_id`, `employee_id`, `date`, `nb_documents`, `valid_amount`) VALUES
-	(1, 4, 1, '2021-10-01', 5, 218),
-	(2, 1, 1, '2021-11-01', 2, 651);
-
 INSERT INTO `standard_fees` (`id`, `name`, `unit_amount`) VALUES
 	(1, 'Nuitée', 80),
 	(2, 'Repas Midi', 29),
 	(3, 'Kilométrage', 2);
+	
+INSERT INTO `state` (`id`, `name`) VALUES
+	(1, 'Créée'),
+	(2, 'Clôturée'),
+	(3, 'Validée'),
+	(4, 'Mise en paiement'),
+	(5, 'Remboursée');
+
+INSERT INTO `fee_sheet` (`id`, `state_id`, `employee_id`, `date`, `nb_documents`, `valid_amount`) VALUES
+	(1, 4, 1, '2021-10-01', 5, 218),
+	(2, 1, 1, '2021-11-01', 2, 651);
 
 INSERT INTO `standard_fees_line` (`id`, `standard_fees_id`, `fee_sheet_id`, `quantity`) VALUES
 	(1, 1, 1, 1),
@@ -22,13 +29,6 @@ INSERT INTO `standard_fees_line` (`id`, `standard_fees_id`, `fee_sheet_id`, `qua
 	(4, 1, 2, 4),
 	(5, 2, 2, 6),
 	(6, 3, 2, 56);
-
-INSERT INTO `state` (`id`, `name`) VALUES
-	(1, 'Créée'),
-	(2, 'Clôturée'),
-	(3, 'Validée'),
-	(4, 'Mise en paiement'),
-	(5, 'Remboursée');
 
 INSERT INTO `variable_fees_line` (`id`, `fee_sheet_id`, `name`, `date`, `amount`) VALUES
 	(1, 1, 'Frais 1', '2021-10-12', 6),
