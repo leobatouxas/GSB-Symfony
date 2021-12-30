0d146be243e59ab1ee6b5aad225538314dcfb997
leobatouxas <leo.batouxas@gmail.com>
2021-12-30 14:46:54 +0100

    Ajout : page d'accueil administration

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
